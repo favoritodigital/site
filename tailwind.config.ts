cd63a8cafefb6160e1bf436b41f0166831fff1d8
Daniel Dalcastagne <daniel.dalcastagne@pixeon.com>
2023-11-23 18:23:29 -0300

    feat: adicionado libs e configurações

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,74 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      colors: {
+        primary: {
+          50: '#ffe1e5',
+          100: '#ffbac2',
+          200: '#ff929f',
+          300: '#fb6e7e',
+          400: '#fa475c',
+          500: '#f3364c',
+          600: '#eb273e',
+          700: '#de1d33',
+          800: '#c22134',
+          900: '#a82433',
+        },
+        secondary: {
+          50: '#f6fbfe',
+          100: '#d3ecfa',
+          200: '#b1dcf4',
+          300: '#92cbeb',
+          400: '#70bbe5',
+          500: '#63afdc',
+          600: '#56a4d1',
+          700: '#4b98c4',
+          800: '#448bb3',
+          900: '#447c9c',
+        },
+        tertiary: {
+          50: '#3539bc',
+          100: '#323697',
+          200: '#2d2f74',
+          300: '#262754',
+          400: '#191a38',
+          500: '#141426',
+          600: '#0d0d16',
+          700: '#040407',
+          800: '#000000',
+          900: '#000000',
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
-export default config
