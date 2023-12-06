dd1d6e418dfb77c60ecb0c35d16ad5ef8c10ebe2
Daniel Dalcastagne <daniel.dalcastagne@pixeon.com>
2023-12-06 16:17:40 -0300

    refactor: adicionado correções sugeridas no PR

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -34,7 +34,7 @@ export const Header = () => {
       <Link className='relative flex h-full w-40' href='/'>
         <Image
           src='/assets/favorito.svg'
-          alt='Favorito - a sua melhor experiência'
+          alt='Favorito - sua melhor experiência'
           objectFit='contain'
           layout='fill'
         />
@@ -63,45 +63,48 @@ export const Header = () => {
         className='transition-height absolute right-0 top-0 z-30 h-0 w-screen bg-gradient-to-br from-primaryDark-85 to-primaryLight-85 duration-300 ease-in-out 
         data-[menuopen=true]:h-screen data-[menuopen=false]:opacity-0 lg:hidden'
       >
-        <div className='p-5'>
-          <Link
-            data-menuopen={isMenuOpen}
-            className='relative flex h-10 w-40 transition-opacity duration-300 data-[menuopen=false]:opacity-0'
-            href='/'
-            onClick={handleMenuClick}
-          >
-            <Image
-              src='/assets/favorito-white.svg'
-              alt='Favorito - a sua melhor experiência'
-              objectFit='contain'
-              layout='fill'
-            />
-          </Link>
-        </div>
         {isMenuOpen && (
-          <div className='flex h-full w-full flex-col items-start justify-center gap-5 px-5 pb-40'>
-            <NavigationButton
-              active={pathname === PagesBadge.home}
-              href={PagesBadge.home}
-              label='Início'
-              variant='mobile'
-              onClick={handleMenuClick}
-            />
-            <NavigationButton
-              active={pathname === PagesBadge.franchiseeLandingPage}
-              href={PagesBadge.franchiseeLandingPage}
-              label='Seja um Franqueado'
-              variant='mobile'
-              onClick={handleMenuClick}
-            />
-            <NavigationButton
-              active={pathname === PagesBadge.establishmentLandingPage}
-              href={PagesBadge.establishmentLandingPage}
-              label='Cadastre seu Estabelecimento'
-              variant='mobile'
-              onClick={handleMenuClick}
-            />
-          </div>
+          <>
+            <div className='p-5'>
+              <Link
+                data-menuopen={isMenuOpen}
+                className='relative flex h-10 w-40 transition-opacity duration-300 data-[menuopen=false]:opacity-0'
+                href='/'
+                onClick={handleMenuClick}
+              >
+                <Image
+                  src='/assets/favorito-white.svg'
+                  alt='Favorito - sua melhor experiência'
+                  objectFit='contain'
+                  layout='fill'
+                />
+              </Link>
+            </div>
+
+            <div className='flex h-full w-full flex-col items-start justify-center gap-5 px-5 pb-40'>
+              <NavigationButton
+                active={pathname === PagesBadge.home}
+                href={PagesBadge.home}
+                label='Início'
+                variant='mobile'
+                onClick={handleMenuClick}
+              />
+              <NavigationButton
+                active={pathname === PagesBadge.franchiseeLandingPage}
+                href={PagesBadge.franchiseeLandingPage}
+                label='Seja um Franqueado'
+                variant='mobile'
+                onClick={handleMenuClick}
+              />
+              <NavigationButton
+                active={pathname === PagesBadge.establishmentLandingPage}
+                href={PagesBadge.establishmentLandingPage}
+                label='Cadastre seu Estabelecimento'
+                variant='mobile'
+                onClick={handleMenuClick}
+              />
+            </div>
+          </>
         )}
       </div>
     </div>
