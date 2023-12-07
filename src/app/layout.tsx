870498073b95b0c7c8de20d3b0203cccf51b0691
Gabriel Moresco <gabriel.moresco25@gmail.com>
2023-12-07 19:33:50 -0300

    feat: adicionado vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Lato } from 'next/font/google'
 
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import { Footer } from '@components/Footer'
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         <div className='pb-10 pt-20'>{children}</div>
         <Footer />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
