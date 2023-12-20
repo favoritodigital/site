f43cf511a4c4d19fa47dab00cd57992790b2539f
Daniel Dalcastagne <daniel.dalcastagne@pixeon.com>
2023-12-19 21:20:29 -0300

    refactor: alterado espaçamento do formulario

--- src/app/seja-franqueado/Section05/index.tsx
+++ src/app/seja-franqueado/Section05/index.tsx
@@ -4,26 +4,24 @@ import { FranchiseeForm } from '@/components/FranchiseeForm'
 export const Section05 = () => {
   return (
     <section className='relative'>
-      <div className='flex w-full justify-center'>
-        <div className='flex flex-col gap-6 p-10 md:px-40'>
-          <h3 className='text-center text-3xl'>
-            Quer ser o{' '}
-            <span className='bg-gradient-to-tr from-primaryDark-400 to-primaryLight-400 bg-clip-text font-semibold text-transparent'>
-              Favorito
-            </span>{' '}
-            da sua cidade?
-          </h3>
-          <FranchiseeForm />
-        </div>
+      <div className='flex w-full flex-col justify-center'>
+        <h3 className='px-10 pt-10 text-center text-3xl'>
+          Quer ser o{' '}
+          <span className='bg-gradient-to-tr from-primaryDark-400 to-primaryLight-400 bg-clip-text font-semibold text-transparent'>
+            Favorito
+          </span>{' '}
+          da sua cidade?
+        </h3>
+        <FranchiseeForm />
       </div>
-      <div className='absolute left-0 top-0 hidden flex-col gap-2 md:flex'>
+      <div className='absolute left-0 top-0 hidden flex-col gap-2 lg:flex'>
         <div className='relative pb-10'>
           <div className='h-28 w-20 bg-secondary-400' />
           <Balloon variant='bl' color='secondary' size='20' className='absolute bottom-0 right-0' />
         </div>
         <Balloon variant='br' color='outlineTertiary' size='20' />
       </div>
-      <div className='absolute bottom-0 right-0 hidden flex-col gap-2 md:flex'>
+      <div className='absolute bottom-0 right-0 hidden flex-col gap-2 lg:flex'>
         <div className='flex gap-2'>
           <Balloon variant='br' color='secondary' size='20' />
           <Balloon variant='bl' color='outlineTertiary' size='20' />
